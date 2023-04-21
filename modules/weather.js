ca121641b14acb8061219d19e79988b30b51c6e8
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-04-21 13:44:36 -0500

    lab 10

--- modules/weather.js
+++ modules/weather.js
@@ -2,6 +2,9 @@
 
 const axios = require('axios');
 
+let cache = {};
+
+
 class Forecast {
   constructor(cityObj) {
     this.date = cityObj.datetime;
@@ -10,20 +13,41 @@ class Forecast {
     this.high_temp = cityObj.high_temp;
   }
 }
+// TODO: create a key for each query
+// TODO: if the results exist AND they are within a valid timeframe... send that data from the cache
+// TODO: if the results do not exist, hit API, and then add that info to the cache
 
 async function getWeather(req, res, next) {
   try {
     let lat = req.query.lat;
     let lon = req.query.lon;
     let searchQuery = req.query.searchQuery;
+    let key = `lat:${lat}-lon:${lon}-weather`;
 
-    let url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&units=I&lat=${lat}&lon=${lon}`;
+    if (cache[key] && (Date.now() - cache[key].timestamp) < 8.64e+7) {
+      console.log('Cash was hit!', cache);
+      res.status(200).send(cache[key].data);
 
-    let foundCity = await axios.get(url);
+    } else {
+      console.log('Cache was missed!');
+      let url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&units=I&lat=${lat}&lon=${lon}`;
 
-    console.log('lat', lat, 'lon', lon, 'searchQuery', searchQuery);
-    let dataToSend = foundCity.data.data.map(newWeather => new Forecast(newWeather));
-    res.status(200).send(dataToSend);
+      let foundCity = await axios.get(url);
+
+      console.log('lat', lat, 'lon', lon, 'searchQuery', searchQuery);
+      let dataToSend = foundCity.data.data.map(newWeather => new Forecast(newWeather));
+
+      cache[key] = {
+        data: dataToSend,
+        timestamp: Date.now()
+      };
+
+      res.status(200).send(dataToSend);
+    }
+
+
+
+
 
   } catch (error) {
     next(error);
